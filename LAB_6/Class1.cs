faa3c1e9be192c359289c1375f63964eab603973
luftkusse <80095098+luftkusse@users.noreply.github.com>
2021-06-05 06:33:27 +0300

    Add files via upload

---
+++ LAB_6/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LAB_6
+{
+    interface IComparable
+    {
+        void CompareTo();
+    }
+    public interface I_Fig
+    {
+        int GetP();
+        int GetA();
+        int GetV();
+    }
+    public class Class1 : I_Fig, IComparable
+    {
+        public int x, y, z;
+        public Class1(int a, int b, int c)
+        {
+            x = a;
+            y = b;
+            z = c;
+        }
+        public void CompareTo()
+        {
+            if (x > z) Console.WriteLine("X > Z");
+            if (z > x) Console.WriteLine("Z > X");
+        }
+        public int GetA()
+        {
+            return x * z;
+        }
+        public int GetP()
+        {
+            return (x + z) * 2;
+        }
+        public int GetV()
+        {
+            return x * y * z;
+        }
+    }
+}
---
+++ LAB_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LAB_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l, w, h;
+            Console.Write("Длина основания: ");
+            l = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ширина основания: ");
+            w = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Высота: ");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            Class1 a = new Class1(l, w, h);
+            Console.WriteLine("Площадь основания: {0}", a.GetA());
+            Console.WriteLine("Периметр основания: {0}", a.GetP());
+            Console.WriteLine("Объём: {0}\n", a.GetV());
+
+        }
+    }
+}
