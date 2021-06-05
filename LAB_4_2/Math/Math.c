870834c25498b271782be17151bc4f905388a88c
luftkusse <80095098+luftkusse@users.noreply.github.com>
2021-06-05 03:58:47 +0300

    Add files via upload

---
+++ LAB_4_2/Math/Math.c
@@ -0,0 +1,13 @@
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+__declspec(dllexport) int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+
+#ifdef __cplusplus
+  }
+#endif
---
+++ LAB_4_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LAB_4_2
+{
+    static class Math
+    {
+        [DllImport("Math.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int Sum(int a, int b);
+
+        [DllImport("Math.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int Minus(int a, int b);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("a = " + a + "; b = " + b);
+            Console.WriteLine("а + b = " + Math.Sum(a, b));
+            Console.WriteLine("a - b = " + Math.Minus(a, b));
+
+
+        }
+    }
+}
