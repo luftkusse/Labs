c62d65000e9bf7a65aa1d06fc2d3e36e7f02070a
luftkusse <80095098+luftkusse@users.noreply.github.com>
2021-06-05 06:33:10 +0300

    Add files via upload

---
+++ LAB_5/Math.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LAB_5
+{
+    public abstract class Math
+    {
+        public abstract int Schet();
+    }
+    public class M1 : Math
+    {
+        public int length, width, height;
+        public M1(int a, int b, int c)
+        {
+            length = a;
+            width = b;
+            height = c;
+        }
+        public override int Schet()
+        {
+            Console.WriteLine("Вы не переопределили метод " + "\"" + (length * width * height) + "\"\n");
+            return (length * width * height);
+        }
+    }
+    public class M2 : Math
+    {
+        public int length, width, height;
+        public M2(int a, int b, int c)
+        {
+            length = a * 2;
+            width = b * 2;
+            height = c * 2;
+        }
+        public override int Schet()
+        {
+            Console.WriteLine("Вы переопределили метод " + "\"" + (length * width * height) + "\"\n");
+            return (length * width * height);
+        }
+
+    }
+}
---
+++ LAB_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int l, w, h;
+
+            Console.Write("Введите длину: ");
+            l = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ширину: ");
+            w = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите высоту: ");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            var Classes = new List<Math>
+            {
+                new M1(l,w,h),
+                new M2(l,w,h)
+            };
+            foreach (var i in Classes) i.Schet();
+
+        }
+    }
+}
